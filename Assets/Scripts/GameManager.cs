60d72dda829314cb361b09abaee1463e839c4ea0
Makar Haustein <makarmaxha@gmail.com>
2024-01-27 12:57:36 +0100

    Added reset button for components

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,19 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     private static GameManager _instance;
+    [SerializeField] private GameObject cpu;
+    [SerializeField] private GameObject ram1;
+    [SerializeField] private GameObject ram2;
+    [SerializeField] private GameObject cpuFan;
+    [SerializeField] private GameObject gpu;
+    
+
+    Vector3 cpuPos;
+    Vector3 ram1Pos;
+    Vector3 ram2Pos;
+    Vector3 cpuFanPos;
+    Vector3 gpuPos;
+    
     public static GameManager Instance
     {
         get
@@ -19,9 +32,22 @@ public class GameManager : MonoBehaviour
     {
         _instance = this;
     }
-    void Start()
-    {
+    private void Start() {
+        cpuPos = cpu.transform.position;
+        ram1Pos = ram1.transform.position;
+        ram2Pos = ram2.transform.position;
+        cpuFanPos = cpuFan.transform.position;
+        gpuPos = gpu.transform.position;
         
     }
-    
+
+    public void ResetMotherboard()
+    {
+        cpu.transform.position = cpuPos;
+        ram1.transform.position = ram1Pos;
+        ram2.transform.position  =  ram2Pos;
+        cpuFan.transform.position = cpuFanPos;
+        gpu.transform.position = gpuPos;
+    }
+
 }
